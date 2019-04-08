afe18219db51a15f966f50418dbe5e7cd6280548
Yaroslav <doctor355@yahoo.com>
2019-04-08 21:21:32 +0300

    JS-str func length

--- simple.js
+++ simple.js
@@ -5,3 +5,9 @@ console.log("some text messege");
 // You can check out Bash ssh-key here
 // Try it to do.
 // Another try to do tis!
+
+// ============================>> JavaScript string functions << ====
+
+// 1.length
+var str = "Jagathish";
+str.length; //9  --> returns the length of the string
