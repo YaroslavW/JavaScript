cb83c4202494340b83e4a2a243ac097a64387cfa
Yaroslav <doctor355@yahoo.com>
2019-04-15 11:34:00 +0300

    JS trimStart()

--- simple.js
+++ simple.js
@@ -16,18 +16,25 @@ str.length; //9  --> returns the length of the string
 // 1 - toUpperCase()
 var str = "Jagathish";
 var str2 = str.toUpperCase();
-console.log(str);//Jagathish
-console.log(str2);//JAGATHISH 
+console.log(str); //Jagathish
+console.log(str2); //JAGATHISH
 
 // 2 - toLowerCase()
 var str = "Jagathish";
 var str2 = str.toLowerCase();
-console.log(str);//Jagathish
-console.log(str2);//jagathish
+console.log(str); //Jagathish
+console.log(str2); //jagathish
 
 // 3 -trim()
-var str = "   Jagathish  ";var trimmedStr = str.trim(); console.log(trimmedStr); //"Jagathish"var str1 = "J  aga";
+var str = "   Jagathish  ";
+var trimmedStr = str.trim();
+console.log(trimmedStr); //"Jagathish"var str1 = "J  aga";
 var trimmedStr1 = str1.trim();
 console.log(trimmedStr1); //"J  aga"var str2 = "J   .";
 var trimmedStr2 = str2.trim();
 console.log(trimmedStr2); //"J   ."
+
+//  4 - trimStart()
+var str = "   Jagathish  ";
+var trimmedStr = str.trimStart();
+console.log(trimmedStr); //"Jagathish  "
